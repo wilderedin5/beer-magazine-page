2925c5158f21de5ca47b763024a8e6ff330a5c29
wilderedin5 <wildnefalem5@gmail.com>
2020-04-23 01:58:49 +0300

    базовая структура дубль 2, вывод пива на витринку

---
+++ src/Components/ProductPage/Beer/Beer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Beer.module.scss';
+
+const Beer = ({image,name,description,date,abv,tips,contributed}) => {
+    return (
+        <div className={style.beer}>
+            <div className={style.beerImage}>
+                <img src={image} alt="" />
+            </div>
+            <div><b>Название:</b> {name}</div>
+            <div><b>Описание:</b> {description}</div>
+            <div><b>Сварен:</b> {date}</div>
+            <div><b>Крепость:</b> {abv}</div>
+            <div><b>Советы от пивоваров:</b> {tips}</div>
+            <div><b>Поставщик:</b> {contributed}</div>
+        </div>
+    )
+}
+
+export default Beer;
--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from './ProductPage.module.scss';
+import Beer from './Beer/Beer';
 
 const ProductPage = (props) => {
     return(
-    <div>
+    <div className={style.ProductPage}>
         {
-            props.beers.map(beer => <div>{beer.name}</div>)
+            props.beers.map(beer => <Beer image={beer.image_url} name={beer.name} description={beer.description} tips={beer.brewers_tips}
+                                        contributed={beer.contributed_by} date={beer.first_brewed} abv={beer.abv} />)
         }
     </div>
     )
