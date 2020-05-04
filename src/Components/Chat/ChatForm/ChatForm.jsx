f85ad2c3af8825ad5a861e5498167ccd48f7a29d
wilderedin5 <wildnefalem5@gmail.com>
2020-05-04 22:54:02 +0300

    small style

--- src/Components/Chat/ChatForm/ChatForm.jsx
+++ src/Components/Chat/ChatForm/ChatForm.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import createFormElement from '../../../hoc/createFromElement/createFormElement';
 import { required, maxAbv260, maxAbv60 } from '../../../Validators/ChatFormValidate';
+import style from './ChatForm.module.scss';
 
 const Input = createFormElement("input");
 const Textarea = createFormElement("textarea");
 
 const ChatForm = (props) => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form className={style.chatForm} onSubmit={props.handleSubmit}>
             <Field component={Textarea} type="text" placeholder="Текст..." name="messageText" validate={[required,maxAbv260]} />
             <Field component={Input} type="text" placeholder="Имя" name="authorName" validate={[required,maxAbv60]} />
             <button>Отправить сообщение</button>
