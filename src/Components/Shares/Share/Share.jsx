79fcaa4ee68e88eeab2c72ad0768c83b899287ab
wilderedin5 <wildnefalem5@gmail.com>
2020-05-06 15:05:38 +0300

    Если акция открыта, она занимает всю ширину, кнопка возврата на страницу акций

--- src/Components/Shares/Share/Share.jsx
+++ src/Components/Shares/Share/Share.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import style from './Share.module.scss';
 import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
 
 const Share = (props) => {
     return (
-        <div className={style.share}>
+        <div className={cn(style.share, {[style.shareOpened]: props.shareOpened})}>
             <div className={style.shareImage}>
                 <img src={props.imageShare} alt="" />
             </div>
             <div><b>Акция: </b>{props.shareName}</div>
             <div><b>Описание: </b>{props.descShare}</div>
             <div><b>Дата проведения: </b>{props.timeShare}</div>
-            { !props.shareOpened &&
+            { !props.shareOpened ?
                 <NavLink to={`/share/${props.id}`}>
                     <button>Перейти на страницу акции</button>
+                </NavLink>
+                :
+                <NavLink to="/share">
+                    <button>Вернуться на страницу акций</button>
                 </NavLink>
             }
         </div>
