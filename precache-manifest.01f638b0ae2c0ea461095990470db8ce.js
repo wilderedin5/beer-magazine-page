1a8a965f41e2b8d01cb271427cbe9ba5f8a3f2b2
wilderedin5 <wildnefalem5@gmail.com>
2020-05-16 15:04:41 +0300

    Updates

---
+++ precache-manifest.01f638b0ae2c0ea461095990470db8ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36d7e64cf9b7ce1b2b2a6d85bb145d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bcb8ee9d2698a89493c",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "42bb47d97ee0c3f5f146",
+    "url": "/static/css/main.3fe95180.chunk.css"
+  },
+  {
+    "revision": "7bcb8ee9d2698a89493c",
+    "url": "/static/js/2.a1033ab3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a1033ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bb47d97ee0c3f5f146",
+    "url": "/static/js/main.39fa9b03.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
