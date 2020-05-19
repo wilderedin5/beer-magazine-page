52d9480c9964115db74ce0d9f925ad8f1440b0b3
wilderedin5 <wildnefalem5@gmail.com>
2020-05-19 11:40:57 +0300

    вывод комментов у акций

---
+++ src/Components/Shares/Share/CommentShare/CommentShare.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import style from './CommentShare.module.scss';
+import { Comment, Tooltip } from 'antd';
+import {DislikeOutlined,LikeOutlined,DislikeFilled} from "@ant-design/icons";
+
+
+const CommentShare = (props) => {
+    const actions = [
+        !props.liked ?
+            <span key="comment-basic-like">
+                <Tooltip title="Like">
+                    {React.createElement(props.liked === false ? LikeOutlined : LikeOutlined, {
+                        onClick: () => props.toggleLikeMessage(props.id, true)
+                    })}
+                </Tooltip>
+                <span className="comment-action">{props.likeCount}</span>
+            </span>
+            :
+            <span key=' key="comment-basic-dislike"'>
+                <Tooltip title="Dislike">
+                    {React.createElement(
+                        props.liked === true ? DislikeFilled : DislikeOutlined,
+                        {
+                            onClick: () => props.toggleLikeMessage(props.id, false)
+                        }
+                    )}
+                </Tooltip>
+                <span className="comment-action">{props.likeCount}</span>
+            </span>,
+            <span onClick={() => props.deleteMessageFromChat(props.id)}>Удалить сообщение</span>
+    ];
+    return (
+        <Comment className={style.message} actions={actions} author={<a>{props.name}</a>} content={<p>{props.text}</p>} />
+    )
+}
+
+export default CommentShare;
+
+
--- src/Components/Shares/Share/Share.jsx
+++ src/Components/Shares/Share/Share.jsx
@@ -3,26 +3,33 @@ import style from './Share.module.scss';
 import { NavLink } from 'react-router-dom';
 import cn from 'classnames';
 import { Button } from 'antd';
+import CommentShare from './CommentShare/CommentShare';
 
 const Share = (props) => {
     return (
-        <div className={cn(style.share, {[style.shareOpened]: props.shareOpened})}>
-            <div className={style.shareImage}>
-                <img src={props.imageShare} alt="" />
+        <div>
+            <div className={cn(style.share, { [style.shareOpened]: props.shareOpened })}>
+                <div className={style.shareImage}>
+                    <img src={props.imageShare} alt="" />
+                </div>
+                <div><b>Акция: </b>{props.shareName}</div>
+                <div><b>Описание: </b>{props.shareOpened ? props.descShareFull : props.descShare}</div>
+                <div><b>Дата проведения: </b>{props.timeShare}</div>
+                {!props.shareOpened ?
+                    <NavLink to={`/share/${props.id}`}>
+                        <Button type="primary">Перейти на страницу акции</Button>
+                    </NavLink>
+                    :
+                    <NavLink to="/share">
+                        <Button type="primary">Вернуться на страницу акций</Button>
+                    </NavLink>
+                }
             </div>
-            <div><b>Акция: </b>{props.shareName}</div>
-            <div><b>Описание: </b>{props.shareOpened ? props.descShareFull : props.descShare}</div>
-            <div><b>Дата проведения: </b>{props.timeShare}</div>
-            { !props.shareOpened ?
-                <NavLink to={`/share/${props.id}`}>
-                    <Button type="primary">Перейти на страницу акции</Button>
-                </NavLink>
-                :
-                <NavLink to="/share">
-                    <Button type="primary">Вернуться на страницу акций</Button>
-                </NavLink>
+            { props.shareOpened &&
+                props.comment.map(comment => <CommentShare {...comment} />)
             }
         </div>
+
     )
 }
 
